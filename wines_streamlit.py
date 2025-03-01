94a235c6b92cb2f9ba28b2cccef5aba6e3c57bce
FILET81 <migimo@gmail.com>
2025-03-01 22:09:48 +0100

    Fourteenth Upload

--- wines_streamlit.py
+++ wines_streamlit.py
@@ -32,7 +32,7 @@ if selection=="Introduction":     # Providing some info about the world of wines
     st.markdown("- There are **18 noble grapes' varieties** -> '*planted **across the Globe**, and producing **high-quality** wines'*:")
     st.markdown("**Red** (lightest to darkest) -> ***Pinot Noir***, *Grenache*, ***Merlot***, *Sangiovese*, *Nebbiolo*, *Tempranillo*, ***Cabernet Sauvignon***, ***Syrah*** and *Malbec*.")
     st.markdown("**White** (lightest to richest) -> *Pinot Gris*, ***Riesling***, ***Sauvignon Blanc***, *Chenin Blanc*, *Moscato*, *Gewürztraminer*, *Sémillon*, *Viognier* and ***Chardonnay***.")
-    #st.image("Images\Noble Varieties.jpg", caption="Noble Varieties", width=650)
+    st.image("https://media.winefolly.com/18-noble-grapes-diagram-winefolly.jpg", caption="Noble Varieties", width=950)
 
 
 
@@ -41,11 +41,49 @@ if selection=="Introduction":     # Providing some info about the world of wines
 elif selection=="Vizzes":     # Embedding "Tableau Public" vizzes
     st.header("Let's play a bit with the data! :grapes:")
 
+    def vizzes():
+        html_temp="""<div class='tableauPlaceholder' id='viz1740833678486' style='position: relative'><noscript><a href='#'><img alt='World of Wines-4 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;wi&#47;wine_vizzes&#47;WorldofWines-4&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='wine_vizzes&#47;WorldofWines-4' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;wi&#47;wine_vizzes&#47;WorldofWines-4&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1740833678486');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.height='877px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
+        components.html(html_temp, width=1500, height=1200)
+
+    if __name__ == '__main__':
+        vizzes()
 
 
 
+elif selection=="Wine Searcher":
 
-elif selection=="Wine Searcher":     # Displaying the wine Searcher out of the treated DataFrame
+    # Charging flags for each country:
+    arg_image = "https://flagpedia.net/data/flags/w1160/ar.webp"
+    aus_image = "https://flagpedia.net/data/flags/w1160/au.webp"
+    chl_image = "https://flagpedia.net/data/flags/w1160/cl.webp"
+    esp_image = "https://flagpedia.net/data/flags/w1160/es.webp"
+    ita_image = "https://flagpedia.net/data/flags/w1160/it.webp"
+    deu_image = "https://flagpedia.net/data/flags/w1160/de.webp"
+    aut_image = "https://flagpedia.net/data/flags/w1160/at.webp"
+    nzl_image = "https://flagpedia.net/data/flags/w1160/nz.webp"
+    zaf_image = "https://flagpedia.net/data/flags/w1160/za.webp"
+    prt_image = "https://flagpedia.net/data/flags/w1160/pt.webp"
+    fra_image = "https://flagpedia.net/data/flags/w1160/fr.webp"
+    usa_image = "https://flagpedia.net/data/flags/w1160/us.webp"
+    
+    #Creating a dictionary to link each country of the dataset with the corresponding flag:
+    flags_dict = {"Argentina":arg_image,
+                  "Australia":aus_image,
+                  "Chile":chl_image,
+                  "Spain":esp_image,
+                  "Italy":ita_image,
+                  "Germany":deu_image,
+                  "Austria":aut_image,
+                  "New Zealand":nzl_image,
+                  "South Africa":zaf_image,
+                  "Portugal":prt_image,
+                  "France":fra_image,
+                  "United States":usa_image}  
+
+    #st.image(countries_dict[row["Country"]], width=50) -> inside the function dentro de la función para que salga el emoji
+    
+    
+    # Displaying the wine Searcher out of the treated DataFrame
     def wine_searcher():
         #from WinesDatasetCleaning import wine_dataset_cleaning as wdc
         # Loading the dataset:
@@ -61,13 +99,14 @@ elif selection=="Wine Searcher":     # Displaying the wine Searcher out of the t
 
         # Coding Streamlit:
         st.header("Let's look for a tasty wine! :face_with_monocle:")
-        st.image("Images\Vineyards-6.jpg", caption="Vineyards somewhere in the World", width=950) 
+        st.image("Images\Vineyards-6.jpg", caption="Vineyards somewhere in the World", width=900) 
 
         # Determining all other filters:
         # Selecting countries with option "All" included:
         selected_country = st.multiselect("Please choose a country from the list:", ["All Countries"] + w_country)
-        if "All Countries" in selected_country:
+        if "All Countries" in selected_country or len(selected_country) == 0:
             selected_country = w_country
+
         if selected_country:
             filtered_country = wines_dataset[wines_dataset["country"].isin(selected_country)]
         else:
@@ -87,10 +126,10 @@ elif selection=="Wine Searcher":     # Displaying the wine Searcher out of the t
             filtered_type = wines_dataset
         
         #
-        selected_monovarietal = st.radio("Would you like a monovarietal wine?", w_monovarietal) == "no"     # "== "yes/no"" avoids empty DataFrame in case user doesn't select
+        selected_monovarietal = st.radio("Would you like a monovarietal wine?", w_monovarietal) == "yes"     # "== "yes/no"" avoids empty DataFrame in case user doesn't select
         
         #
-        selected_noble = st.radio("Would you like a noble grape variety?", w_noble) == "no"     # "== "yes/no"" avoids empty DataFrame in case user doesn't select
+        selected_noble = st.radio("Would you like a noble grape variety?", w_noble) == "yes"     # "== "yes/no"" avoids empty DataFrame in case user doesn't select
         
         # Selecting wine vintages with option "All" included:
         selected_vintage = st.multiselect("Please choose a vintage from the list:", ["All Vintages"] + w_vintage)
@@ -104,16 +143,18 @@ elif selection=="Wine Searcher":     # Displaying the wine Searcher out of the t
         ### Filtering the dataset in order to have a dynamic "st.slider":
         filtered_data = wines_dataset[
                 (wines_dataset["country"].isin(selected_country) if selected_country else True) &     # "if selected_type else True" avoids empty DataFrame in case user doesn't select
-                (wines_dataset["wine_type"].isin(selected_type) if selected_type else True) &     # "if selected_type else True" avoids empty DataFrame in case user doesn't select
+                (wines_dataset["wine_type"].isin(selected_type)if selected_country else True)  &     # "if selected_type else True" avoids empty DataFrame in case user doesn't select
                 (wines_dataset["monovarietal"]==selected_monovarietal) &
                 (wines_dataset["noble_international"]==selected_noble) &
-                (wines_dataset["vintage"].isin(selected_vintage) if selected_vintage else True)     # "if selected_type else True" avoids empty DataFrame in case user doesn't select
+                (wines_dataset["vintage"].isin(selected_vintage)if selected_country else True)     # "if selected_type else True" avoids empty DataFrame in case user doesn't select
                 ]     
         
         # Determining "min" and "max" price based on the filtered data:
         if not filtered_data.empty:
             w_min_price = filtered_data["price_usd"].min()
             w_max_price = filtered_data["price_usd"].max()
+            if w_min_price == w_max_price:
+                w_max_price = w_min_price + 1     # Adjustment to avoid slider issues!
         else:
             w_min_price = wines_dataset["price_usd"].min()
             w_max_price = wines_dataset["price_usd"].max()
@@ -125,32 +166,39 @@ elif selection=="Wine Searcher":     # Displaying the wine Searcher out of the t
             st.progress(100)
             wines_found = filtered_data[
                 filtered_data["price_usd"] < selected_max_price
-                ].sort_values(by=["points", "price_usd", "vintage"], ascending=[False, False, False])#.head(15)
-
+                ].sort_values(by=["points", "price_usd", "vintage"], ascending=[False, False, False])
             if not wines_found.empty:
                 st.success("Congrats, wines matching your criteria are found! :smiley:\n"
-                           "Find below a list of the 15 best wines according to such criteria:")
+                           "Find below a list of the best wines according to such criteria:")
 
-                # Reseting index and setting it up in order to start from 1:
-                wines_found.reset_index(drop=True, inplace=True)
-                wines_found.index = np.arange(1, len(wines_found) + 1)
-
-                # Showing the resulting DataFrame:
-                st.dataframe(wines_found[["points",
-                                          "price_usd",
-                                          "vintage",
-                                          "country",
-                                          "winery",
-                                          "apellation",
-                                          "wine_type",
-                                          "variety",                                          
-                                          "taste_dry-sweet",
-                                          "taste_body",
-                                          "taste_tannins",
-                                          "taste_acidity",
-                                          "avg_abv_%",
-                                          "avg_serve_temp_c",
-                                          "primary_flavors"]])
+                col1, col2 = st.columns(2)
+                counter = 0
+                for index, row in wines_found.head(10).iterrows():    
+                    with col1:                    
+                        counter += 1
+                        st.header(f"{counter}. *Wine Name*")
+                        st.image(flags_dict[row["country"]], width=30)
+                        st.write(f"**Country:** {row["country"]}")
+                        st.write(f"**Points:** {row["points"]}")
+                        st.write(f"**Price:** ${row["price_usd"]} USD")
+                        st.write(f"**Vintage:** {row["vintage"]}")
+                        st.write(f"**Apellation:** {row["apellation"]}")
+                        st.write(f"**Type:** {row["wine_type"]}")
+                        st.write(f"**Variety:** {row["variety"]}")
+                        st.markdown("---")
+                        
+                    with col2:
+                        st.header("Tasting Notes")
+                        st.image(flags_dict[row["country"]], width=30)
+                        st.write(f"**Rating Sweetness:** {row["taste_dry-sweet"]}")
+                        st.write(f"**Rating Body:** {row["taste_body"]}")
+                        st.write(f"**Rating Tannins:** {row["taste_tannins"]}")
+                        st.write(f"**Rating Acidity:** {row["taste_acidity"]}")
+                        st.write(f"**ABV (Avg):** {row["avg_abv_%"]} %")
+                        st.write(f"**Service Temperature (Avg):** {row["avg_serve_temp_c"]}° Celsius")
+                        st.write(f"**Primary Flavors:** {row["primary_flavors"]}")
+                        st.markdown("---")
+                
             else:
                 st.error("Pity, no wines are found with these criteria! :disappointed_relieved:\n"
                          "Try to modify them for a successful search!")
@@ -190,6 +238,7 @@ else:     # Closing the Presentation
     st.markdown("https://mavenanalytics.io/")
     st.markdown("https://winefolly.com/")
     st.markdown("https://www.wikipedia.org/")
+    st.markdown("https://flagpedia.net/")
     st.markdown("https://www.oiv.int/")
     st.markdown("https://www.oecd.org/")
     st.markdown("And many others...")
